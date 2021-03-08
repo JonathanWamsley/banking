5d680831c52eb3d2e1dc5fc71e71fff8141a1e42
jonny <jonnywamsley@github.com>
2021-03-08 07:40:00 -0500

    adding roles validation to the auth middleware

--- app/auth_middleware.go
+++ app/auth_middleware.go
@@ -15,13 +15,14 @@ type AuthMiddleware struct {
 func (a AuthMiddleware) authorizationHandler() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			currentRoute := mux.CurrentRoute(r)
 			currentRouteVars := mux.Vars(r)
 			authHeader := r.Header.Get("Authorization")
 
 			if authHeader != "" {
 				token := getTokenFromHeader(authHeader)
 
-				isAuthorized := a.repo.IsAuthorized(token, currentRouteVars)
+				isAuthorized := a.repo.IsAuthorized(token, currentRoute.GetName(), currentRouteVars)
 				if isAuthorized {
 					next.ServeHTTP(w, r)
 				} else {
